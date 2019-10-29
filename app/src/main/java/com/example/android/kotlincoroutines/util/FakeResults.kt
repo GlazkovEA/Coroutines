2450244e174880ad287a07460db5d19a2342941a
Evgenii Glazkov <homounikumus1@gmail.com>
2019-10-29 22:10:15 +0500

    first

---
+++ app/src/main/java/com/example/android/kotlincoroutines/util/FakeResults.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.kotlincoroutines.util
+
+/**
+ * A list of fake results our network can return.
+ */
+val FAKE_RESULTS = listOf(
+        "https://www.dropbox.com/s/6oalc54xpyn693a/1.jpg?dl=1",
+        "https://www.dropbox.com/s/ptk2pbcr154rkpl/5869ae125599f498bfdc2808f206fcb2.jpg?dl=1",
+        "https://www.dropbox.com/s/6oalc54xpyn693a/1.jpg?dl=1",
+        "https://www.dropbox.com/s/9zm7nbqodg1bo0t/4.jpg?dl=1",
+        "https://www.dropbox.com/s/1ok4pps4mth49e7/5.jpg?dl=1",
+        "https://www.dropbox.com/s/rrs8lloyd20184y/6.jpg?dl=1",
+
+        "https://www.dropbox.com/s/h0swrhqp9v29kl2/7.jpg?dl=1",
+        "https://www.dropbox.com/s/n5d3xzkyzuyncxi/8.jpg?dl=1"
+)
